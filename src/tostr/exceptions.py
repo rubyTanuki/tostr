b713b3e6cc28f10d9069299bbbc028751d775eca
rubyTanuki <161648546+rubyTanuki@users.noreply.github.com>
2026-06-13 22:48:54 -0500

    rem unused exceptions

--- src/tostr/exceptions.py
+++ src/tostr/exceptions.py
@@ -4,19 +4,7 @@ class TostrError(Exception):
     """Base exception for all Tostr domain errors."""
     pass
 
-class StructNotFoundError(TostrError):
-    pass
-
 class APIKeyError(TostrError):
-    pass
-
-class ResolveError(TostrError):
-    pass
-
-class LanguageNotSupportedError(TostrError):
-    pass
-
-class TargetFileNotFoundError(TostrError):
     pass
 
 class DatabaseNotFoundError(TostrError):
